7d170bfba5bd3bea94730ea1776c65ebc9b286b2
Karthick Rajamurugan <karthick.rajamurugan@safehealth.me>
2024-10-28 22:06:39 +0530

    Added left icon

--- src/icons/icons.tsx
+++ src/icons/icons.tsx
@@ -1,75 +1,43 @@
-import { default as AddIconMui } from '@mui/icons-material/Add';
-import { default as ArrowForwardIconMui } from '@mui/icons-material/ArrowForward';
-import { default as DeleteForeverIconMui } from '@mui/icons-material/DeleteForever';
+import AddIcon from '@mui/icons-material/Add';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
 import DownloadOutlinedIcon from '@mui/icons-material/DownloadOutlined';
 import DragIndicatorIcon from '@mui/icons-material/DragIndicator';
-import { default as EditIconMui } from '@mui/icons-material/Edit';
+import EditIcon from '@mui/icons-material/Edit';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 import KeyboardReturnIcon from '@mui/icons-material/KeyboardReturn';
-import { default as PreviewIconMui } from '@mui/icons-material/Preview';
-import { default as SaveIconMUI } from '@mui/icons-material/Save';
+import PreviewIcon from '@mui/icons-material/Preview';
+import SaveIcon from '@mui/icons-material/Save';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import ExpandLessIcon from '@mui/icons-material/ExpandLess';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import CodeIcon from '@mui/icons-material/Code';
+import MonitorIcon from '@mui/icons-material/Monitor';
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
+import TitleIcon from '@mui/icons-material/Title';
 
-export const PreviewIcon = ({ }) => (
-    <PreviewIconMui />
-);
-
-export const ArrowForwardIcon = ({ }) => (
-    <ArrowForwardIconMui />
-);
-
-export const SaveIcon = ({ }) => (
-    <SaveIconMUI />
-);
-
-export const EditIcon = ({ }) => (
-    <EditIconMui />
-);
-
-export const DeleteIcon = ({ }) => (
-    <DeleteOutlineIcon />
-);
-
-export const DeleteForeverIcon = ({ }) => (
-    <DeleteForeverIconMui />
-);
-
-export const UpArrow = ({ }) => (
-    <KeyboardArrowUpIcon />
-);
-
-export const DownArrow = ({ }) => (
-    <KeyboardArrowDownIcon />
-);
-
-export const DragIcon = ({ }) => (
-    <DragIndicatorIcon />
-);
-
-export const ReturnIcon = ({ }) => (
-    <KeyboardReturnIcon />
-);
-
-export const DownloadIcon = ({ }) => (
-    <DownloadOutlinedIcon />
-);
-
-export const AddIcon = ({ }) => (
-    <AddIconMui />
-);
-
-export const ExpandIcon = ({ }) => (
-    <ExpandMoreIcon />
-);
-
-export const CollapseIcon = ({ }) => (
-    <ExpandLessIcon />
-);
-
-export const RightChevronIcon = ({ }) => (
-    <ChevronRightIcon />
-);
+export {
+    PreviewIcon,
+    ArrowForwardIcon,
+    SaveIcon,
+    EditIcon,
+    DeleteOutlineIcon as DeleteIcon,
+    DeleteForeverIcon,
+    KeyboardArrowUpIcon as UpArrow,
+    KeyboardArrowDownIcon as DownArrow,
+    DragIndicatorIcon as DragIcon,
+    KeyboardReturnIcon as ReturnIcon,
+    DownloadOutlinedIcon as DownloadIcon,
+    AddIcon,
+    ExpandMoreIcon as ExpandIcon,
+    ExpandLessIcon as CollapseIcon,
+    ChevronRightIcon as RightChevronIcon,
+    ChevronLeftIcon as LeftChevronIcon,
+    CodeIcon,
+    MonitorIcon,
+    ContentCopyIcon,
+    TitleIcon,
+};
